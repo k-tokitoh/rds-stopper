2cf1aa4b831d990961b63e34f697fe222acca4cb
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-01-23 15:52:15 +0900

    stop DB instance

--- src/functions/main/handler.ts
+++ src/functions/main/handler.ts
@@ -1,9 +1,14 @@
 import "source-map-support/register";
 import { Handler } from "aws-lambda";
 import { middyfy } from "@libs/lambda";
+import * as RDS from "aws-sdk/clients/rds";
 
 const handler: Handler = async () => {
-  console.log("executing.");
+  const rds = new RDS();
+  await rds
+    .stopDBInstance({ DBInstanceIdentifier: "test" })
+    .promise()
+    .catch((err) => console.error(err));
 };
 
 export const main = middyfy(handler);
